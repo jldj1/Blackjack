f24ebc74c3209f5bf213fa846e7abee22381a8ad
jldj1 <davidsonj1@sfusd.edu>
2021-12-01 20:54:26 -0800

    update

--- src/components/testblackjack.py
+++ src/components/testblackjack.py
@@ -1,9 +1,12 @@
 from components.deckComponent import DeckComponent
+from network.users.users import UserModel
 class TestBlackJack:
-    def __init__(self, screen, player, dealer):
+    # add balance
+    def __init__(self, screen, player, dealer, username, users_balance):
         self.screen = screen
+        self.username = username
         self.can_bet = True
-
+        self.balance = int(users_balance)
         self.deck = DeckComponent(self.screen, 240, 30)
         self.won = False
         self.round_started = False
@@ -11,17 +14,27 @@ class TestBlackJack:
         self.ready_to_start_round = False
         self.started = False
         self.done_betting = False
-
         self.player = player
         self.dealer = dealer
         self.ended = False
         self.bet = 0
 
+
     def draw(self):
         self.deck.draw()
 
     def addBet(self, bet):
+        #temp bet 200 + 100
+        #balance: 100
+        # 100 - 300
+        #temp_bet = self.bet + bet
+        #temp_balance = self.balance - bet
+        #print("temp bet: " + str(temp_bet))
+        if self.balance - bet < 0:
+            return f"Current bet: {self.bet}"
         self.bet += bet
+        self.balance = self.balance - bet
+
         self.ready_to_start_round = True
         return f"Current bet: {self.bet}"
 
@@ -38,6 +51,8 @@ class TestBlackJack:
                 self.dealer.addCard(temp_card.suit, temp_card.name)
 
     def stand(self):
+        if not self.started:
+            return
         self.dealer.flipCard(1)
         self.stand_check = True
 
@@ -57,18 +72,27 @@ class TestBlackJack:
         if player_val > dealer_val:
             output = "YOU WON!"
 
+            self.balance = self.balance + self.bet * 2
+
         elif player_val < dealer_val:
             output = "YOU LOST!"
+            #balance = self.balance - self.bet
         else:
             output = "Its a tie!"
-
+            self.balance = self.balance + self.bet
         self.ended = True
+        UserModel.save_user_info(self.username, self.balance)
         return output
 
+
     def hit(self):
+        if not self.started:
+            return
         self.dealPlayer(1)
 
     def startRoundDeal(self):
+        if not self.ready_to_start_round:
+            return
         self.dealDealer(2)
         self.dealPlayer(2)
 
@@ -85,3 +109,10 @@ class TestBlackJack:
         self.stand_check = False
         self.bet = 0
         self.ended = False
+        #if self.deck.total_cards < 5:
+        #    print("Shuffling...")
+        #    self.deck = DeckComponent(self.screen, 240, 30)
+
+    def getBalance(self):
+        return self.balance
+
